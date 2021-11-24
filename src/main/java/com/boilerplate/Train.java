1542a8ab18801e845401043cf33b79b211f5457c
CallToon <75041867+CallToon@users.noreply.github.com>
2021-11-24 21:15:53 +0100

    Train class aangemaakt

---
+++ src/main/java/com/boilerplate/Train.java
@@ -0,0 +1,14 @@
+package com.boilerplate;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Train {
+    public List<Person> people;
+    public List<Station> stations;
+
+    public Train() {
+        this.people = new LinkedList<>();
+        this.stations = new LinkedList<>();
+    }
+}
