c3821c5efdab74fd4a05cfdfcb6654e9130e0927
foodsnotgood <joroeder2588@gmail.com>
2021-11-24 21:15:15 +0100

    station toegevoegd

---
+++ src/main/java/com/boilerplate/Station.java
@@ -0,0 +1,14 @@
+package com.boilerplate;
+
+import java.util.List;
+
+public class Station {
+    public String name;
+    public List<Person> people;
+
+    public Station(String name, List<Person> people) {
+        this.name = name;
+        this.people = people;
+    }
+
+}
